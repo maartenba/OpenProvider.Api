6652f747c963cd76d38c862694db1fa5557b1a66
Maarten Balliauw <maarten@maartenballiauw.be>
2020-04-22 20:02:03 +0200

    Initial commit

---
+++ OpenProvider.Api/OpenProvider.Api.cs
@@ -0,0 +1,79 @@
+// ReSharper disable once CheckNamespace
+
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OpenProvider.Api
+{
+    public partial interface IApiClient
+    {
+        /// <summary>Login with username and password, and store the authentication token for future requests with this client.</summary>
+        /// <returns>A successful response.</returns>
+        /// <exception cref="ApiException">A server side error occurred.</exception>
+        Task<AuthLoginResponse> LoginAndStoreAsync(AuthLoginRequest body);
+        
+        /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
+        /// <summary>Login with username and password, and store the authentication token for future requests with this client.</summary>
+        /// <returns>A successful response.</returns>
+        /// <exception cref="ApiException">A server side error occurred.</exception>
+        Task<AuthLoginResponse> LoginAndStoreAsync(AuthLoginRequest body, CancellationToken cancellationToken);
+        
+        /// <summary>
+        /// Logout and get rid of the stored access token.
+        /// </summary>
+        void Logout();
+    }
+
+    public partial class ApiClient
+    {
+        private string _accessToken;
+
+        public ApiClient(HttpClient httpClient, string accessToken = null)
+            : this(httpClient)
+        {
+            _accessToken = accessToken;
+        }
+
+        partial void PrepareRequest(HttpClient client, HttpRequestMessage request, string url)
+        {
+            if (_accessToken != null)
+            {
+                request.Headers.Authorization = AuthenticationHeaderValue.Parse("Bearer " + _accessToken);
+            }
+        }
+
+        partial void PrepareRequest(HttpClient client, HttpRequestMessage request, System.Text.StringBuilder urlBuilder)
+        {
+            if (_accessToken != null)
+            {
+                request.Headers.Authorization = AuthenticationHeaderValue.Parse("Bearer " + _accessToken);
+            }
+        }
+
+        public Task<AuthLoginResponse> LoginAndStoreAsync(AuthLoginRequest body)
+        {
+            return LoginAndStoreAsync(body, System.Threading.CancellationToken.None);
+        }
+
+        public async Task<AuthLoginResponse> LoginAndStoreAsync(AuthLoginRequest body,
+            CancellationToken cancellationToken)
+        {
+            Logout();
+
+            var result = await LoginAsync(body, cancellationToken);
+            if (result.Code == 0 && !string.IsNullOrEmpty(result.Data.Token))
+            {
+                _accessToken = result.Data.Token;
+            }
+
+            return result;
+        }
+
+        public void Logout()
+        {
+            _accessToken = null;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# OpenProvider.Api
+
+A .NET library to access OpenProvider API's. Generated from Swagger document at https://docs.openprovider.com/swagger.json.
+
+## Getting Started
+
+Documentation of the API is [available from OpenProvider](https://docs.openprovider.com/).
+
+To get started with the library, make use of `ApiClient` and the `LoginAndStoreAsync` method to access the API.
